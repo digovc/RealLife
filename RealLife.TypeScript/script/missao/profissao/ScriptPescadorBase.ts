c73fe7919c35e9828f6eac65cfd1afd0dd7e9183
Rodrigo Daros <rodrigo.conspena@gmail.com>
2017-05-03 15:47:15 -0300

    - Criação das classes das primeiras profissões.

---
+++ RealLife.TypeScript/script/missao/profissao/ScriptPescadorBase.ts
@@ -0,0 +1,28 @@
+/// <reference path="ScriptProfissaoBase.ts"/>
+
+module RealLife
+{
+    // #region Importações
+    // #endregion Importações
+
+    // #region Enumerados
+    // #endregion Enumerados
+
+    export abstract class ScriptPescadorBase extends ScriptProfissaoBase
+    {
+        // #region Constantes
+        // #endregion Constantes
+
+        // #region Atributos
+        // #endregion Atributos
+
+        // #region Construtores
+        // #endregion Construtores
+
+        // #region Métodos
+        // #endregion Métodos
+
+        // #region Eventos
+        // #endregion Eventos
+    }
+}
---
+++ RealLife.TypeScript/script/missao/profissao/ScriptProfissaoBase.ts
@@ -0,0 +1,28 @@
+/// <reference path="../ScriptMissaoBase.ts"/>
+
+module RealLife
+{
+    // #region Importações
+    // #endregion Importações
+
+    // #region Enumerados
+    // #endregion Enumerados
+
+    export abstract class ScriptProfissaoBase extends ScriptMissaoBase
+    {
+        // #region Constantes
+        // #endregion Constantes
+
+        // #region Atributos
+        // #endregion Atributos
+
+        // #region Construtores
+        // #endregion Construtores
+
+        // #region Métodos
+        // #endregion Métodos
+
+        // #region Eventos
+        // #endregion Eventos
+    }
+}
