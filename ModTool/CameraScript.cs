d59b07b1786c2c5b8f268733f157d90f85297bee
Rodrigo Daros <rodrigo.conspena@gmail.com>
2017-05-08 09:20:26 -0300

    - Implementação de uma ferramenta para montagem das missões.

---
+++ ModTool/CameraScript.cs
@@ -0,0 +1,70 @@
+﻿using GTA;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ModTool
+{
+    public class CameraScript : ScriptBase
+    {
+        #region Constantes
+
+        private const string DIR_CAMERA_INFO = "camera_info.txt";
+
+        #endregion Constantes
+
+        #region Atributos
+
+        #endregion Atributos
+
+        #region Construtores
+
+        #endregion Construtores
+
+        #region Métodos
+
+        protected override void setEventos()
+        {
+            base.setEventos();
+
+            this.KeyUp += this.onKeyUp;
+        }
+
+        private void copiarInfo(KeyEventArgs arg)
+        {
+            var stbInfo = new StringBuilder();
+
+            if (File.Exists(DIR_CAMERA_INFO))
+            {
+                stbInfo.AppendLine(File.ReadAllText(DIR_CAMERA_INFO));
+            }
+
+            stbInfo.AppendLine("Câmera info:");
+            stbInfo.AppendLine(string.Format("Posição: x: {0}, y: {1}, z: {2}", GameplayCamera.Position.X, GameplayCamera.Position.Y, GameplayCamera.Position.Z));
+            stbInfo.AppendLine(string.Format("Rotação: x: {0}, y: {1}, z: {2}", GameplayCamera.Rotation.X, GameplayCamera.Rotation.Y, GameplayCamera.Rotation.Z));
+
+            File.WriteAllText(DIR_CAMERA_INFO, stbInfo.ToString());
+        }
+
+        private void processarOnKeyUp(KeyEventArgs arg)
+        {
+            switch (arg.KeyCode)
+            {
+                case Keys.C:
+                    this.copiarInfo(arg);
+                    return;
+            }
+        }
+
+        #endregion Métodos
+
+        #region Eventos
+
+        private void onKeyUp(object sender, KeyEventArgs arg)
+        {
+            this.processarOnKeyUp(arg);
+        }
+
+        #endregion Eventos
+    }
+}
---
+++ ModTool/ScriptBase.cs
@@ -0,0 +1,46 @@
+﻿using GTA;
+
+namespace ModTool
+{
+    public abstract class ScriptBase : Script
+    {
+        #region Constantes
+
+        #endregion Constantes
+
+        #region Atributos
+
+        #endregion Atributos
+
+        #region Construtores
+
+        public ScriptBase()
+        {
+            this.iniciar();
+        }
+
+        #endregion Construtores
+
+        #region Métodos
+
+        protected virtual void inicializar()
+        {
+        }
+
+        protected virtual void setEventos()
+        {
+        }
+
+        private void iniciar()
+        {
+            this.inicializar();
+            this.setEventos();
+        }
+
+        #endregion Métodos
+
+        #region Eventos
+
+        #endregion Eventos
+    }
+}
